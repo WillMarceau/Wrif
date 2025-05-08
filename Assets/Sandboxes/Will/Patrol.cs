dbaa0aeb133a92289c1b968559ca8f78187b28bf
Will Marceau <willfmarceau@gmail.com>
2025-05-07 22:15:40 -0700

    created patrol script

--- Assets/Sandboxes/Will/Patrol.cs
+++ Assets/Sandboxes/Will/Patrol.cs
@@ -5,21 +5,42 @@ public class Patrol : MonoBehaviour
     // waypoints
     public Transform[] patrolPoints;
     // navMesh Agent
-    UnityEngine.AI.NavMeshAgent navMeshAgent;
+    //UnityEngine.AI.NavMeshAgent navMeshAgent;
     // current waypoint index
-    int index;
+    // movement stuff
+    public float speed;
+    int index = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         // set first waypoint as destination
-        navMeshAgent.SetDestination(patrolPoints[0].position);
+        Vector3 direction = patrolPoints[index].position - transform.position;
+        direction.y = 0;
+        Quaternion rot = Quaternion.LookRotation(direction);
+        transform.rotation = rot;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // if the patrol point is reached, move to next waypoint
-        
-        
+        // move towards target direction
+        // can change target when player is detected, otherwise just move between waypoints
+        Transform target = patrolPoints[index];
+        Vector3 direction;
+
+        // if waypoint reached, set next waypoint and turn towards it
+        if (Vector3.Distance(transform.position, target.position) < 0.1f) {
+            index = (index + 1) % patrolPoints.Length;
+            target = patrolPoints[index];
+            direction = target.position - transform.position;
+
+            // rotate
+            direction = (target.position - transform.position).normalized;
+            Quaternion rot = Quaternion.LookRotation(direction);
+            transform.rotation = rot;
+
+        }
+        direction = (target.position - transform.position).normalized;
+        transform.position += direction * speed * Time.deltaTime;
     }
 }
