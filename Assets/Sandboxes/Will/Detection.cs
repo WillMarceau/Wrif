c0460c7745c47d6284a8e0e2c68c2af737625be1
Will Marceau <willfmarceau@gmail.com>
2025-05-08 17:20:36 -0700

    added drone detection and explosion

---
+++ Assets/Sandboxes/Will/Detection.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public abstract class Detection : MonoBehaviour
+{
+    // this script create the interface for drone detection
+    public abstract void PlayerDetected(Transform Player);
+}
